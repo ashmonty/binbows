62a92540dc850bf662fc00dcf28df42d390b7949
montylion <hey@montylion.dev>
2021-07-15 23:20:31 +0200

    Update WIP text

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import "98.css";
 import Favicon from "../components/head";
 import Taskbar from "../components/taskbar";
 
+import styles from "../styles/Home.module.css";
+
 import InterwebsNavigator from "../components/programs/interwebs";
 
 export default function Home() {
@@ -15,14 +17,22 @@ export default function Home() {
         <title>Michaelsoft Binbows</title>
       </Head>
       <section>
-        <p>
-          In case you couldn't tell, this is still very much a work in progress.
-        </p>
-        <p>Click on the Internet Explorer icon in the taskbar!</p>
-        <Link href="/qwertyuiop">Here's a 404 page if you'd like.</Link>
-          <span>
-            <InterwebsNavigator className="interwebs" />
-          </span>
+        <div className={styles.WIPtext}>
+          <p>
+            In case you couldn't tell, this is still very much a WIP (work in
+            progress)
+          </p>
+          <p>
+            If you wanna see something that I personally find pretty cool, click
+            on the Internet Explorer icon in the taskbar!
+          </p>
+          <p>
+            Also, <Link href="/qwertyuiop">here</Link>'s a 404 page if you'd
+            like.
+          </p>
+        </div>
+
+        <InterwebsNavigator className="interwebs" />
       </section>
       <Taskbar />
     </home>

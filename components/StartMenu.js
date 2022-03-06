cf830a52608b0ea75f0a7539ec6218b92691ce7c
Ash Monty <git@ashmonty.com>
2022-03-06 21:03:18 +0100

    feat: add 98.css, make start menu

---
+++ components/StartMenu.js
@@ -0,0 +1,22 @@
+import styles from "../styles/components/StartMenu.module.css";
+import "98.css"
+
+export default function StartMenu(props) {
+  const { startState, setStartState } = props;
+  if (startState) {
+    return (
+      <>
+        <div className={styles.gutter} onClick={() => setStartState(false)} />
+        <div className={styles.startMenu}>
+          <div className={styles.sidebar}>
+            <p>
+              <span>Binbows</span>98
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
+}
--- components/Taskbar.js
+++ components/Taskbar.js
@@ -1,6 +1,11 @@
-import styles from "../styles/components/Taskbar.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import classNames from "classnames";
+
+import styles from "../styles/components/Taskbar.module.css";
+import "98.css";
+
+import Start from "./StartMenu";
 
 export default function Taskbar() {
   const [clock, setClock] = useState(
@@ -8,7 +13,7 @@ export default function Taskbar() {
   );
   setInterval(
     () =>
-    setClock(
+      setClock(
         new Date().toLocaleTimeString([], {
           hour: "2-digit",
           minute: "2-digit",
@@ -17,20 +22,33 @@ export default function Taskbar() {
     1000
   );
 
+  const [startState, setStartState] = useState(false);
+
   return (
-    <div className={styles.taskbar}>
-      <div className={styles.startButton}>
-        <Image
-          src="/assets/images/icons/system/start.png"
-          width="16"
-          height="16"
-          alt="Start icon"
-        />
-        <span className={styles.text}>Start</span>
+    <>
+      <div className={styles.taskbar}>
+        <button
+          className={classNames({
+            [styles.startButton]: true,
+            [styles.active]: startState,
+          })}
+          onClick={() => setStartState(!startState)}
+        >
+          <Image
+            src="/assets/images/icons/system/start.png"
+            width="16"
+            height="16"
+            alt="Start icon"
+          />
+          <span className={styles.text}>Start</span>
+        </button>
+        <span className={styles.separator} />
+        <span className={styles.handle} />
+        <div className={styles.taskManager} id="task-manager"></div>
+        <span className={styles.clock}>{clock}</span>
       </div>
-      <span className={styles.separator} />
-      <span className={styles.handle} />
-      <span className={styles.clock}>{clock}</span>
-    </div>
+
+      <Start startState={startState} setStartState={setStartState} />
+    </>
   );
 }

1d6f5ca02a2e4d98b49acf0610c095d309c9d47f
Ash Monty <git@ashmonty.com>
2022-03-07 21:23:51 +0100

    feat: add rudimentary drag support

--- components/WindowFrame.js
+++ components/WindowFrame.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import classNames from "classnames";
+import Draggable from "react-draggable";
 
 import "98.css";
 import styles from "/styles/components/WindowFrame.module.css";
@@ -11,7 +12,6 @@ export default function WindowFrame({
   staticIndex,
   windows,
   children,
-  style,
 }) {
   function closeWindow() {
     const updatedWindows = windows?.filter(
@@ -21,34 +21,37 @@ export default function WindowFrame({
   }
 
   return (
-    <div
-      className={classNames({
-        window: true,
-        [styles.window]: true,
-        maximized: maximized,
-      })}
-      style={
-        style || {
+    <Draggable
+      handle=".title-bar"
+      defaultPosition={{x: 0, y: 0}}
+    >
+      <div
+        className={classNames({
+          window: true,
+          [styles.window]: true,
+          maximized: maximized,
+        })}
+        style={{
           position: "absolute",
           top: (staticIndex + 1) * 24,
           left: (staticIndex + 1) * 24,
-        }
-      }
-    >
-      <div className="title-bar">
-        {icon ? (
-          <Image src={icon} width="16px" height="16px" alt="Window icon" />
-        ) : null}
-        <div className="title-bar-text">
-          {title} {maximized.toString()}
+        }}
+      >
+        <div className="title-bar">
+          {icon ? (
+            <Image src={icon} width="16px" height="16px" alt="Window icon" />
+          ) : null}
+          <div className="title-bar-text">
+            {title} {maximized.toString()}
+          </div>
+          <div className="title-bar-controls">
+            <button aria-label="Minimize"></button>
+            <button aria-label={maximized ? "Restore" : "Maximize"}></button>
+            <button aria-label="Close" onClick={closeWindow}></button>
+          </div>
         </div>
-        <div className="title-bar-controls">
-          <button aria-label="Minimize"></button>
-          <button aria-label={maximized ? "Restore" : "Maximize"}></button>
-          <button aria-label="Close" onClick={closeWindow}></button>
-        </div>
+        <div className="window-body">{children}</div>
       </div>
-      <div className="window-body">{children}</div>
-    </div>
+    </Draggable>
   );
 }
